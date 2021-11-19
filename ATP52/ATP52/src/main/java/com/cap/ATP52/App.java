ce09d8a0777bab2543359fcccd5037bfc6e0957b
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-11-19 09:25:43 -0300

    ATP52

---
+++ ATP52/ATP52/src/main/java/com/cap/ATP52/App.java
@@ -0,0 +1,18 @@
+package com.cap.ATP52;
+
+import javax.swing.JFrame;
+
+import com.cap.ATP52.view.CategoriaView;
+
+/**
+ * JFrame application
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	CategoriaView vc2 = new CategoriaView();
+        vc2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    }
+}
---
+++ ATP52/ATP52/src/main/java/com/cap/ATP52/dao/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package com.cap.ATP52.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
+	private DataSource dataSource;
+
+    public ConnectionFactory() {
+        ComboPooledDataSource pool = new ComboPooledDataSource();
+        pool.setJdbcUrl("jdbc:postgresql://localhost:5433/postgres");
+        pool.setUser("postgres");
+        pool.setPassword("123456");
+        pool.setMaxPoolSize(10);
+        this.dataSource = pool;
+    }
+
+    public Connection getConnection() throws SQLException{        
+        return this.dataSource.getConnection();
+    }
+
+}
